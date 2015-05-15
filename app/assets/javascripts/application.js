f503a2d3efcbff5bd250a28b89ba37052fd20a09
Marc Beitchman <mbeitchman@gmail.com>
2015-05-15 11:13:23 +0200

    I add structure and content to the website

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,10 @@
 //= require bootstrap.min
 //= require turbolinks
 //= require_tree .
+
+$( "#ux-wire-button" )
+  .mouseover(function() {
+    alert("yo");
+  })
+  .mouseout(function() {
+  });
